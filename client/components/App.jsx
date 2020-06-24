0e90a64bb519bd3d63fffeb228095c4cb3f52263
Nick-Jarvie <nickjarvie@gmail.com>
2020-06-24 18:43:08 +1200

    get kanye quote api working and css

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import request from 'superagent'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+class App extends React.Component {
+  state = {
+    quote: ''
+  }
+
+  componentDidMount() {
+    this.getMyQuote()
+  }
+
+  handleClick = () => {
+    this.getMyQuote()
+  }
+
+  getMyQuote = () => {
+    request.get('https://api.kanye.rest')
+      .then(res => {
+        this.setState({
+          quote: res.body.quote
+        })
+      })
+  }
+
+  render() {
+    return (
+      <>
+      <div className='body'>
+        <h1>
+            {/* <pre className='body'> */}
+              KANYE
+              QUOTE
+              GENER
+              -ATOR
+               
+              
+            {/* </pre> */}
+          </h1>
+        {/* <button className='button' onClick={this.handleClick}>Click here!</button> */}
+        <p className='quote' onClick={this.handleClick}>{this.state.quote}</p>
+      </div>
+      </>
+    )
+  }
 }
 
 export default App
